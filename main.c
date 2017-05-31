be9670101298cf037e3c7a9852971709821eb6d1
Lane Terry <lrterry@mbp.local>
2017-05-31 12:13:17 -0400

    Add input handling

--- main.c
+++ main.c
@@ -1,6 +1,14 @@
 #include <stdio.h>
 
 int main(int argc, char** argv) {
+	if(argc < 2) {
+		printf("Error, one argument required.\nPlease enter a word to be printed to the console.\n");
+		return 1;
+	} else if(argc > 2) {
+		printf("Error, too many arguments. This program only requires a single argument.\n");
+		return 2;
+	}
+	
 	printf("%s\n", argv[1]);
 	return 0;
 } 
